a6de98de616ca9cc4a2fc398e410ff5c0883ee7a
Herque Maxime <118621660+R3tr0oO@users.noreply.github.com>
2023-04-24 10:45:05 +0200

    Add files via upload

---
+++ Contact.php
@@ -0,0 +1,46 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>/Contact</title>
+</head>
+<body class="body">
+
+    <div class="conteneur-general">
+        <div class="effetneonHeader">
+            <a href="index.html" class="a">/Home</a>/Contact
+        </div>
+    </div>
+    <br>
+
+    <div class="conteneur-general">
+
+        <form class="form" target="_blank" rel="noopener noreferrer" action="https://formsubmit.co/maxime.herque@outlook.com" method="POST">
+
+          <div>
+            <input type="text" class="cyberpunk" name="name" placeholder="Full Name" required/>
+          </div>
+          <div>
+            <input type="email" class="cyberpunk" name="email" placeholder="Email Address" required/>
+          </div>
+          <div>
+            <textarea placeholder="Your Message" class="cyberpunk" name="message" rows="5" required></textarea>
+          </div>
+            <br><br>
+          <button type="submit" class="cybr-btn">
+                Submit<span aria-hidden></span>
+                <span aria-hidden class="cybr-btn__glitch">-0 // - \[ 0 .. /</span>
+                <span aria-hidden class="cybr-btn__tag">SI0</span>
+          </button>
+        </form>
+    </div>
+    <div class="conteneur-footer">
+        <div class="effetneonFooter">
+                <a href="Legal_notices.html" class="a"> Legal Notices </a>
+                <hr class="hr">
+        </div>    
+    </div>
+</body>
+</html>
